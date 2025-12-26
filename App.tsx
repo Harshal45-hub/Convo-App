72248e0a52e7d51819ac14662e13ca6a4b837d8e
Harshal Sonar <sonarharshal18@gmail.com>
2025-12-26 15:58:10 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,45 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import HomePage from './screens/HomePage'
+import BottomTabs from './Navigation/BottomTabs'
+import { NavigationContainer } from '@react-navigation/native'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
+  const [text, setText] = useState('')
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+    <NavigationContainer>
+      <BottomTabs />
+    </NavigationContainer>
+  )
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+export default App
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({})
---
+++ screens/HomePage.tsx
@@ -0,0 +1,112 @@
+import { StyleSheet, Text, Touchable, TouchableOpacity, View ,TextInput, ScrollView} from 'react-native'
+import React from 'react'
+import { widthToDP, heightToDP } from 'react-native-responsive-screens'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+
+
+
+const HomePage = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#1E1E1E', }}>
+      <View style={styles.headingContainer}>
+        <View style={styles.mainContainer}>
+          <View style={styles.navbarContainer}>
+            <Text style={styles.navbarText}>Find Your Tribe</Text>
+            <Text style={styles.descriptionText}>Connect with people who share your interests!</Text>
+          </View>
+          <TouchableOpacity style={styles.iconStyle}>
+            <Ionicons name='notifications-outline' size={(heightToDP('4%'))} color='white' />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.searchBarContainer}>
+          <Ionicons name='search' size={(widthToDP('6%'))} color='#A9A9A9' style={styles.searchIcon} />
+          <TextInput placeholder='Search for groups, events, or topics' placeholderTextColor='#A9A9A9' style={styles.TextInputStyle} />
+        </View>
+      </View>
+      <ScrollView>
+        <View style={styles.contentContainer}></View>
+        <View style={styles.contentContainer}></View>
+        <View style={styles.contentContainer}></View>
+        <View style={styles.contentContainer}></View>
+        <View style={styles.contentContainer}></View>
+        <View style={styles.contentContainer}></View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default HomePage
+
+const styles = StyleSheet.create({
+  headingContainer:{
+    width: widthToDP('100%'),
+    height: heightToDP('23%'),
+    backgroundColor:'#1E1E1E',
+    zIndex:10,
+    elevation:5
+  },
+  mainContainer: {
+    flexDirection: 'row',
+    width: widthToDP('100%'),
+    height: heightToDP('11%'),
+    marginTop: heightToDP('2%'),
+    alignItems: 'center',
+  },
+
+  navbarContainer: {
+    width: '80%',
+    height: '90%',
+    alignSelf: 'flex-start',
+  },
+  navbarText: {
+    fontFamily: 'Poppins-Condensed',
+    color: 'white',
+    fontSize: heightToDP('3.5%'),
+    fontWeight: 'bold',
+    marginLeft: '5%'
+  },
+  descriptionText: {
+    fontFamily: 'Poppins-Bold',
+    color: 'white',
+    fontSize: heightToDP('1.7%'),
+    marginLeft: '5%'
+  },
+  iconStyle: {
+    position: 'absolute',
+    right: widthToDP('4%'),
+    top: heightToDP('2%'),
+  },
+  searchBarContainer:{
+    flexDirection:'column',
+    width: widthToDP('90%'),
+    height: heightToDP('6%'),
+    backgroundColor: '#2E2E2E',
+    alignSelf:'center',
+    borderRadius: widthToDP('15%'),
+    marginTop: heightToDP('2%'),
+    justifyContent:'center',
+  },
+  searchIcon:{
+    position: 'absolute',
+    marginLeft: widthToDP('5%')
+  },
+  TextInputStyle:{
+     color: 'white',
+     paddingLeft: 10, 
+     height: '100%',
+     marginLeft: widthToDP('10%')  
+  },
+  contentContainer:{
+    width: widthToDP('100%'),
+    height:heightToDP('20%'),
+    backgroundColor:'#3E3E3E',
+    marginTop: heightToDP('2%')
+  }
+
+
+
+})
+
